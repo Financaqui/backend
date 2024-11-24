f75afd202640bc9493d0de387a4acadadcb616fd
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2024-11-23 21:00:34 -0300

    feat: creating project

---
+++ src/main/java/com/finacaqui/Financaqui/FinancaquiApplication.java
@@ -0,0 +1,13 @@
+package com.finacaqui.Financaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancaquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finacaqui/Financaqui/FinancaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finacaqui.Financaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
