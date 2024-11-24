69685f32a51bd19253610c8f02e0ac1f27f4f8cd
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2024-11-23 21:12:28 -0300

    feat: creating the interface GastoRepository

---
+++ src/main/java/com/finacaqui/Financaqui/repository/GastoRepository.java
@@ -0,0 +1,7 @@
+package com.finacaqui.Financaqui.repository;
+
+import com.finacaqui.Financaqui.model.Gasto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GastoRepository extends CrudRepository<Gasto, Long> {
+}
