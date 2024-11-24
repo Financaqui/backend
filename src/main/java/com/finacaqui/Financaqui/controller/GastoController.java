ee8b048187761d4b989a42aca246959230e18620
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2024-11-23 21:08:26 -0300

    feat: creating the class GastoController

---
+++ src/main/java/com/finacaqui/Financaqui/controller/GastoController.java
@@ -0,0 +1,10 @@
+package com.finacaqui.Financaqui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("gasto")
+public class GastoController {
+
+}
