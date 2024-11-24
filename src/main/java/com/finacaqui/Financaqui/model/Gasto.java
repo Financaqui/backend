98a02384f1a79af6fc01bd6397ee40355f8edb80
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2024-11-23 21:06:13 -0300

    feat: creating the entity Gasto

---
+++ src/main/java/com/finacaqui/Financaqui/model/Gasto.java
@@ -0,0 +1,24 @@
+package com.finacaqui.Financaqui.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Entity(name = "gasto")
+@Table(name = "gasto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Gasto {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoria;
+    private Float valor;
+    private Integer qtdParcelas;
+    private Timestamp data;
+
+}
